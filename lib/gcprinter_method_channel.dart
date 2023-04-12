82aeb66d03fe2789a7a77d869605b934693a2d7d
goodcom6 <117073785+goodcom6@users.noreply.github.com>
2023-04-12 14:27:21 +0800

    Add files via upload

---
+++ lib/gcprinter_method_channel.dart
@@ -0,0 +1,71 @@
+import 'dart:collection';
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import 'gcprinter_platform_interface.dart';
+
+/// An implementation of [GcprinterPlatform] that uses method channels.
+class MethodChannelGcprinter extends GcprinterPlatform {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = const MethodChannel('Gcprinter');
+
+  @override
+  void printJson(String json) {
+    methodChannel.invokeMethod("printJson", {"json": json});
+  }
+
+  @override
+  void printImageFile(String filePath, int align, bool isAutoFeed) {
+    var map = HashMap<String, Object>();
+    map["file_path"] = filePath;
+    map["align"] = align;
+    map["auto_feed"] = isAutoFeed;
+    methodChannel.invokeMethod("printImageFile", map);
+  }
+
+  @override
+  void printImage(Uint8List img, int align, bool isAutoFeed) {
+    var map = HashMap<String, Object>();
+    map["data"] = img;
+    map["align"] = align;
+    map["auto_feed"] = isAutoFeed;
+    methodChannel.invokeMethod("printImage", map);
+  }
+
+  @override
+  void drawText(String strLeft, int fontLeft, String strMid, int fontMid,
+      String strRight, int fontRight) {
+    var map = HashMap<String, Object>();
+    map["strLeft"] = strLeft;
+    map["fontLeft"] = fontLeft;
+    map["strMid"] = strMid;
+    map["fontMid"] = fontMid;
+    map["strRight"] = strRight;
+    map["fontRight"] = fontRight;
+    methodChannel.invokeMethod("drawText", map);
+  }
+
+  @override
+  void drawBarcode(String str, int align, int type) {
+    var map = HashMap<String, Object>();
+    map["data"] = str;
+    map["align"] = align;
+    map["type"] = type;
+    methodChannel.invokeMethod("drawBarcode", map);
+  }
+
+  @override
+  void printText(bool isAutoFeed) {
+    var map = HashMap<String, Object>();
+    map["auto_feed"] = isAutoFeed;
+    methodChannel.invokeMethod("printText", map);
+  }
+
+  @override
+  Future<bool> isDeviceSupport() async {
+    final bool isOk = await methodChannel.invokeMethod('isDeviceSupport');
+    return isOk;
+  }
+}
---
+++ lib/gcprinter_platform_interface.dart
@@ -0,0 +1,56 @@
+import 'dart:typed_data';
+
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+import 'gcprinter_method_channel.dart';
+
+abstract class GcprinterPlatform extends PlatformInterface {
+  /// Constructs a GcprinterPlatform.
+  GcprinterPlatform() : super(token: _token);
+
+  static final Object _token = Object();
+
+  static GcprinterPlatform _instance = MethodChannelGcprinter();
+
+  /// The default instance of [GcprinterPlatform] to use.
+  ///
+  /// Defaults to [MethodChannelGcprinter].
+  static GcprinterPlatform get instance => _instance;
+
+  /// Platform-specific implementations should set this with their own
+  /// platform-specific class that extends [GcprinterPlatform] when
+  /// they register themselves.
+  static set instance(GcprinterPlatform instance) {
+    PlatformInterface.verifyToken(instance, _token);
+    _instance = instance;
+  }
+
+  void printJson(String json) {
+    throw UnimplementedError('printJson() has not been implemented');
+  }
+
+  void printImageFile(String filePath, int align, bool isAutoFeed) {
+    throw UnimplementedError('printImageFile() has not been implemented');
+  }
+
+  void printImage(Uint8List img, int align, bool isAutoFeed) {
+    throw UnimplementedError('printImage() has not been implemented');
+  }
+
+  void drawText(String strLeft, int fontLeft, String strMid, int fontMid,
+      String strRight, int fontRight) {
+    throw UnimplementedError('drawText() has not been implemented');
+  }
+
+  void drawBarcode(String str, int align, int type) {
+    throw UnimplementedError('drawBarcode() has not been implemented');
+  }
+
+  void printText(bool isAutoFeed) {
+    throw UnimplementedError('printText() has not been implemented');
+  }
+
+  Future<bool> isDeviceSupport() {
+    throw UnimplementedError('isDeviceSupport() has not been implemented');
+  }
+}
